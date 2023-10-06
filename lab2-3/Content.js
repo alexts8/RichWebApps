d431119f6de39096faf7c16e2b35426beeaf8a56
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-06 17:03:40 +0100

    colour, links and alerts

--- lab2-3/Content.js
+++ lab2-3/Content.js
@@ -1,28 +1,41 @@
 //array of images
-let catsImages = [
-    "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-    "https://e3.365dm.com/21/03/768x432/skynews-cats-missing-microchip_5315182.jpg?20210323142004",
-    "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc3836660-7846-11eb-80c3-8cc375faed89.jpg?crop=5729%2C3222%2C187%2C805&resize=1200",
-    "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/07/petting_pet_cat-1296x728-header.jpg?w=1155&h=1528",
-	"https://lh3.googleusercontent.com/pw/AL9nZEXYJlrVkYoKIkpx07_3F4HOiTiOheaoaiRAcwrUg3C613-jkzEubJ3k8Z9fDjG5IfVqCzorphZ00vp6mIyB79GtCsoyV69xXe9cqrA0zglgrcvYhH2UP4cDR88WTm1AmuyCxQHAWCB5JzKD7eD94dtNZA=w690-h920-no"
+let rainbowImages = [
+    "https://img.freepik.com/free-vector/pride-gradient-1_78370-282.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQ2UeRHzDx_Vz9TnJKTBi_2Dy8IQ5WN6sxA&usqp=CAU",
+    "https://static.vecteezy.com/system/resources/thumbnails/006/911/398/small/rainbow-waves-background-free-vector.jpg"
 ];
 
 //reverse through array of images
 //getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
 const imgs = document.getElementsByTagName("img");
 for(let i = 0; i < imgs.length; i++) {
-    const randomImg = Math.floor(Math.random() * catsImages.length)
-    imgs[i].src = catsImages[randomImg]
+    const randomImg = Math.floor(Math.random() * rainbowImages.length)
+    imgs[i].src = rainbowImages[randomImg]
 }
 //do the same for h1 elements
 const headers = document.getElementsByTagName("h1");
 for (let i = 0; i < headers.length; i++){
-    headers[i].innerText = "Cats are awesome.";
+    headers[i].innerText = "Cool Website!";
+    headers[i].style.color = "Pink"
 }
 //do the same for p elements
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++){
-    p[i].innerText = "This website is now about cats.";
+    p[i].innerText = "Cool website!";
+    p[i].style.color = "Pink"
 }
+
+const href = document.getElementsByTagName("href");
+for (let i = 0; i < href.length; i++){
+    yourElement.setAttribute('href', 'https://www.pexels.com/search/dog/');
+}
+
+const div = document.getElementsByTagName("div");
+for (let i = 0; i < div.length; i++){
+    div[i].style.backgroundImage = "https://img.freepik.com/free-vector/pride-gradient-1_78370-282.jpg";
+}
+
+
+alert("this website was boring so i made it interesting")
 
 
