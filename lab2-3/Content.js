a3a4eb0f8823ee1266605fbe253d319030c4a492
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-06 17:27:58 +0100

    lots of changes - alerts and choice

--- lab2-3/Content.js
+++ lab2-3/Content.js
@@ -5,6 +5,51 @@ let rainbowImages = [
     "https://static.vecteezy.com/system/resources/thumbnails/006/911/398/small/rainbow-waves-background-free-vector.jpg"
 ];
 
+let boringImages = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS03EhgdfIHYI5-1ZrveghONeRHnFwmvskeeg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQj8p5OqrffesDGXAxQiqWV8Fsz2LEbm_06g&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT34evipC01KZo6K-WuxnTjYyNuJ28uNQjlkA&usqp=CAU"
+];
+
+
+if(confirm("would you like to make this website interesting?"))
+{
+    const imgs = document.getElementsByTagName("img");
+    for(let i = 0; i < imgs.length; i++) {
+        const randomImg = Math.floor(Math.random() * rainbowImages.length)
+        imgs[i].src = rainbowImages[randomImg]
+    }
+
+    //do the same for h1 elements
+    const headers = document.getElementsByTagName("h1");
+    for (let i = 0; i < headers.length; i++){
+        headers[i].innerText = "You Made it Interesting!";
+        headers[i].style.color = "Pink"
+    }
+
+    const headers2 = document.getElementsByTagName("h2");
+    for (let i = 0; i < headers2.length; i++){
+        headers2[i].innerText = "Cool Website!";
+        headers2[i].style.color = "Pink"
+    }
+
+    const headers3 = document.getElementsByTagName("h3");
+    for (let i = 0; i < headers3.length; i++){
+        headers3[i].innerText = "Cool Website!";
+        headers3[i].style.color = "Pink"
+    }
+
+    //do the same for p elements
+    const p = document.getElementsByTagName("p");
+    for (let i = 0; i < p.length; i++){
+        p[i].innerText = "Cool website!";
+        p[i].style.color = "Pink"
+        p[i].style.fontSize = "40px"
+    }
+
+}
+else
+{
 //reverse through array of images
 //getting random image from the array we created before (we use math.floor and math.random to grab a random index in the array)
 const imgs = document.getElementsByTagName("img");
@@ -12,22 +57,36 @@ for(let i = 0; i < imgs.length; i++) {
     const randomImg = Math.floor(Math.random() * rainbowImages.length)
     imgs[i].src = rainbowImages[randomImg]
 }
+
 //do the same for h1 elements
 const headers = document.getElementsByTagName("h1");
 for (let i = 0; i < headers.length; i++){
-    headers[i].innerText = "Cool Website!";
-    headers[i].style.color = "Pink"
+    headers[i].innerText = "Boooo Boring";
+    headers[i].style.color = "Black"
 }
+
+const headers2 = document.getElementsByTagName("h2");
+for (let i = 0; i < headers2.length; i++){
+    headers2[i].innerText = "Boring Website";
+    headers2[i].style.color = "Black"
+}
+
+const headers3 = document.getElementsByTagName("h3");
+for (let i = 0; i < headers3.length; i++){
+    headers3[i].innerText = "Boring Website";
+    headers3[i].style.color = "Black"
+}
+
 //do the same for p elements
 const p = document.getElementsByTagName("p");
 for (let i = 0; i < p.length; i++){
-    p[i].innerText = "Cool website!";
-    p[i].style.color = "Pink"
+    p[i].innerText = "Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla";
+    p[i].style.color = "Black"
 }
 
-const href = document.getElementsByTagName("href");
-for (let i = 0; i < href.length; i++){
-    yourElement.setAttribute('href', 'https://www.pexels.com/search/dog/');
+const link = document.getElementsByTagName("link");
+for (let i = 0; i < link.length; i++){
+    link.href = "https://www.pexels.com/search/dog/";
 }
 
 const div = document.getElementsByTagName("div");
@@ -35,7 +94,9 @@ for (let i = 0; i < div.length; i++){
     div[i].style.backgroundImage = "https://img.freepik.com/free-vector/pride-gradient-1_78370-282.jpg";
 }
 
+}
 
-alert("this website was boring so i made it interesting")
+
+
 
 
