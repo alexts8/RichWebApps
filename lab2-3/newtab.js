08a1bee7ab3054d6a464c9990c3645a66430ece0
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-06 16:33:42 +0100

    deleted all that it didnt work

--- lab2-3/newtab.js
+++
@@ -1,9 +0,0 @@
-function openTab(){
-    
-    var newTab = browser.tabs.create({
-        url:'https://en.wikipedia.org/wiki/Dog',
-        active:true
-    })
-}
-
-browser.browserAction.onClicked.addListener(openTab)
