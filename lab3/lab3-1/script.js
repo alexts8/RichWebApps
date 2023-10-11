14e27c42bb0c0e079e6aa1135a0f85ab95c9cafc
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-11 18:42:34 +0100

    more validation

--- lab3/lab3-1/script.js
+++ lab3/lab3-1/script.js
@@ -10,13 +10,29 @@ document.addEventListener('DOMContentLoaded', function ()
 
         addContact.addEventListener('click', function () 
             {
-                if (name === '' || mobile === '' || email === '') {
+ 
+                if (name == '' || number == '' || email == '') {
                     errorContainer.textContent = 'ERROR: All fields are required';
                     return;
                 }
+                
+                if (!name.match(/^[A-Za-z ]{1,20}$/)) {
+                    errorContainer.textContent = 'ERROR: Name must contain only letters and spaces and be 1-20 characters long';
+                    return;
+                }
+        
+                if (!number.match(/^\d{10}$/)) {
+                    errorContainer.textContent = 'ERROR: Please enter a valid phone number';
+                    return;
+                }
+        
+                if (!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/) || email.length >= 40) {
+                    errorContainer.textContent = 'ERROR: Please enter a valid email';
+                    return;
+                }
             }
+
         )
 
-    
-    }
-)
+        clearFields();
+})
