1e2ac01199c1c76386d17e00de7c5039c5c57c6c
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-11 19:12:59 +0100

    validation done

--- lab3/lab3-1/script.js
+++ lab3/lab3-1/script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function ()
         const nameVal = document.getElementById('name');
         const numberVal = document.getElementById('number');
         const emailVal = document.getElementById('email');
-        const errorContainer = document.getElementById('errorContainer');
+        const error = document.getElementById('error');
         const addContact = document.getElementById('addContact');
 
 
@@ -16,26 +16,26 @@ document.addEventListener('DOMContentLoaded', function ()
                 const email = emailVal.value.trim();
  
                 if (name === '' || number === '' || email === '') {
-                    errorContainer.textContent = 'ERROR: All fields are required';
+                    error.textContent = 'ERROR: All fields are required';
                     return;
                 }
                 
                 if (!name.match(/^[A-Za-z ]{1,20}$/)) {
-                    errorContainer.textContent = 'ERROR: Name must contain only letters and spaces and be 1-20 characters long';
+                    error.textContent = 'ERROR: Name must contain only letters and spaces and be 1-20 characters long';
                     return;
                 }
         
                 if (!number.match(/^\d{10}$/)) {
-                    errorContainer.textContent = 'ERROR: Please enter a valid phone number';
+                    error.textContent = 'ERROR: Please enter a valid phone number';
                     return;
                 }
         
                 if (!email.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/) || email.length >= 40) {
-                    errorContainer.textContent = 'ERROR: Please enter a valid email';
+                    error.textContent = 'ERROR: Please enter a valid email';
                     return;
                 }
 
-                errorContainer.textContent='';
+                error.textContent='';
             }
 
         )
