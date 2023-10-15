619c21f680eb861a87f5bbe9ed53202dc0928bba
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-15 16:47:43 +0100

    filtering by 6 word titles

--- lab3/lab3-2/script.js
+++ lab3/lab3-2/script.js
@@ -2,5 +2,11 @@ const fetchData = (url) => fetch(url).then((response) => response.json());
 
 fetchData('http://jsonplaceholder.typicode.com/posts')
     .then((data) => {
-        console.log(data);
+        var titles =  data
+        .filter((post) => post.title.split(' ').length > 6)
+        .map((post) => post.title);
+
+
+    console.log(titles);
+
     });
