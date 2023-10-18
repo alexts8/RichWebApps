be1be9789f1a1ffa0c967a735565167b09717240
Alex Tsiogas <alextsiogas@gmail.com>
2023-10-18 14:26:58 +0100

    profile pic and username working

--- lab3/lab3-3/script.js
+++ lab3/lab3-3/script.js
@@ -1,6 +1,34 @@
 const scrollableList = document.querySelector('.scrollable-list');
 const listItems = scrollableList.querySelectorAll('.list-item');
 
-if (listItems.length <= 5) {
-    scrollableList.style.overflowY = 'auto';
+
+async function getUserProfile() {
+
+    const input_username = document.getElementById("search-bar").value;
+    const profilePicture = document.getElementById("profile-picture");  
+   // const name = document.getElementById("name");  
+    const username = document.getElementById("username");  
+    //const email = document.getElementById("email");  
+   // const location = document.getElementById("location");  
+    //const gists = document.getElementById("gists");  
+
+
+
+        const response = await fetch(`https://api.github.com/users/${input_username}`);
+        if (response.ok) {
+            const userData = await response.json();
+            const avatarURL = userData.avatar_url;
+            const u_username = userData.login;
+            /*const u_email = userData.avatar_url;
+            const u_location = userData.avatar_url;
+            const u_gists = userData.*/
+           
+
+            profilePicture.innerHTML = `<img src="${avatarURL}"width="150">`;
+            username.innerHTML = `<p>${u_username}</p>`;
+        } 
+}
+
+if (scrollableList.childElementCount > 5) {
+    scrollableList.style.overflowY = "scroll";
 }
