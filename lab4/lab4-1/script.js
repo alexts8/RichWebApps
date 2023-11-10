b87e20295a3416eb951b3dff6890cd215b6a317c
Alex Tsiogas <alextsiogas@gmail.com>
2023-11-10 13:07:21 +0000

    rxjs for createnote

--- lab4/lab4-1/script.js
+++ lab4/lab4-1/script.js
@@ -1,43 +1,54 @@
-function createNote() {
-            const noteHeader = document.getElementById('noteHeader').value;
-            const noteBody = document.getElementById('noteBody').value;
-            const noteColor = document.getElementById('noteColor').value;
+document.addEventListener('DOMContentLoaded', function () {
+    const createNoteButton = document.getElementById('createNoteBtn');
 
-            const note = document.createElement('div');
-            note.className = 'note';
-            note.style.backgroundColor = noteColor;
+    const createNoteClick$ = Rx.fromEvent(createNoteButton, 'click');
 
-            const header = document.createElement('h3');
-            header.textContent = noteHeader;
-            
-            const body = document.createElement('p');
-            body.textContent = noteBody;
+    createNoteClick$.subscribe(() => {
+        createNote();
+    });
 
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.onclick = function() {
-                notesContainer.removeChild(note);
-            };
+    function createNote() {
+                const noteHeader = document.getElementById('noteHeader').value;
+                const noteBody = document.getElementById('noteBody').value;
+                const noteColor = document.getElementById('noteColor').value;
 
-            const editButton = document.createElement('button');
-            editButton.textContent = 'Edit';
-            editButton.onclick = function() {
-                const newHeader = prompt('Edit Note Header:', noteHeader);
-                const newBody = prompt('Edit Note Body:', noteBody);
-                if (newHeader !== null && newBody !== null) {
-                    header.textContent = newHeader;
-                    body.textContent = newBody;
-                }
-            };
+                const note = document.createElement('div');
+                note.className = 'note';
+                note.style.backgroundColor = noteColor;
 
-            note.appendChild(header);
-            note.appendChild(body);
-            note.appendChild(deleteButton);
-            note.appendChild(editButton);
+                const header = document.createElement('h3');
+                header.textContent = noteHeader;
+                
+                const body = document.createElement('p');
+                body.textContent = noteBody;
 
-            const notesContainer = document.getElementById('notesContainer');
-            notesContainer.appendChild(note);
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'Delete';
+                deleteButton.onclick = function() {
+                    notesContainer.removeChild(note);
+                };
 
-            document.getElementById('noteHeader').value = '';
-            document.getElementById('noteBody').value = '';
-        }
+                const editButton = document.createElement('button');
+                editButton.textContent = 'Edit';
+                editButton.onclick = function() {
+                    const newHeader = prompt('Edit Note Header:', noteHeader);
+                    const newBody = prompt('Edit Note Body:', noteBody);
+                    if (newHeader !== null && newBody !== null) {
+                        header.textContent = newHeader;
+                        body.textContent = newBody;
+                    }
+                };
+
+                note.appendChild(header);
+                note.appendChild(body);
+                note.appendChild(deleteButton);
+                note.appendChild(editButton);
+
+                const notesContainer = document.getElementById('notesContainer');
+                notesContainer.appendChild(note);
+
+                document.getElementById('noteHeader').value = '';
+                document.getElementById('noteBody').value = '';
+
+            }
+    });
